ac13cfcb2b653cf3e2a4940bcd9effa8ad2eae31
Sophia Xie <xiesophia7@gmail.com>
2026-01-26 23:11:45 -0500

    Add page icons and top bar

---
+++ dorsiflexx/app/(tabs)/booklet.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function BookletScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Booklet Page</Text>
+    </View>
+  );
+}
